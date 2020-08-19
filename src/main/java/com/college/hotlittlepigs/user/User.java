fc273f3ebeefd75e9bd520df375bbf9bb1e6e8c8
Jean Moraes <jeanjms.1999@gmail.com>
2020-08-19 14:34:14 -0300

    Create user entity

---
+++ src/main/java/com/college/hotlittlepigs/user/User.java
@@ -0,0 +1,51 @@
+package com.college.hotlittlepigs.user;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.college.hotlittlepigs.user.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity(name="user")
+@Table(name="user")
+public class User implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 75, nullable = false)
+    private String name;
+
+    @Column(length = 75, nullable = false, unique = true)
+    private String email;
+
+    @Getter(onMethod = @__({@JsonIgnore}))
+    @Setter(onMethod = @__({@JsonProperty}))
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(length = 20, nullable = false, updatable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
