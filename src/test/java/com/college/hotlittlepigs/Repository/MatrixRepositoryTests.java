ec21008ab6cc25c7305afdb92311a761d6b046e2
Jean Moraes <jeanjms.1999@gmail.com>
2020-10-07 17:23:38 -0300

    Adding the repository layer and his tests

---
+++ src/test/java/com/college/hotlittlepigs/Repository/MatrixRepositoryTests.java
@@ -0,0 +1,53 @@
+package com.college.hotlittlepigs.Repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import com.college.hotlittlepigs.matrix.Matrix;
+import com.college.hotlittlepigs.matrix.MatrixRepository;
+import com.college.hotlittlepigs.model.PageRequestModel;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.DEFAULT)
+@SpringBootTest
+public class MatrixRepositoryTests {
+    
+    @Autowired private MatrixRepository matrixRepository;
+
+    @Test
+    public void saveTest(){
+        Matrix newMatrix = new Matrix(null, 39, true);
+        Matrix createdMatrix = matrixRepository.save(newMatrix);
+
+        assertThat(createdMatrix.getId()).isEqualTo(1L);    
+    }
+
+    @Test
+    public void findAllTest(){
+        PageRequestModel prm = new PageRequestModel();
+        Page<Matrix> page = matrixRepository.findAll(prm.toSpringPageRequest());
+        
+        assertThat(page.getTotalElements()).isEqualTo(1);
+    }
+
+    @Test
+    public void getByIdTest() {
+        Optional<Matrix> result = matrixRepository.findById(1L);
+        Matrix matrix = result.get();
+
+        assertThat(matrix.getNumber()).isEqualTo(39);
+    }
+
+
+
+}
