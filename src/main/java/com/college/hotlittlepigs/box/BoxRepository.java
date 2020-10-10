87b89cb3864a30b55ab9c6a377acbcf8fdbec628
Jean Moraes <jeanjms.1999@gmail.com>
2020-10-10 16:51:37 -0300

    Adding the query findByNumberByStatus on Box Repository, and adjust his tests

--- src/main/java/com/college/hotlittlepigs/box/BoxRepository.java
+++ src/main/java/com/college/hotlittlepigs/box/BoxRepository.java
@@ -1,9 +1,15 @@
 package com.college.hotlittlepigs.box;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoxRepository extends JpaRepository<Box, Long>{
+    
+    @Query("SELECT U FROM box U WHERE number=?1 and status=?2")
+    public Optional<Box> findByNumberByStatus(int number, Boolean status);
     
 }
