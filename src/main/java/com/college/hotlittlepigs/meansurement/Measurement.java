74ef5667c7b916993c149394ad6ca719ed44e128
Kammradt <vinicius.kammradt1@gmail.com>
2020-11-15 15:22:23 -0300

    chore: :art: refactor on Measurement module

--- src/main/java/com/college/hotlittlepigs/meansurement/Measurement.java
+++ src/main/java/com/college/hotlittlepigs/meansurement/Measurement.java
@@ -1,39 +1,29 @@
 package com.college.hotlittlepigs.meansurement;
 
-import java.io.Serializable;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
 import com.college.hotlittlepigs.sensors.Sensors;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Table(name = "measurement")
-@Entity(name= "measurement")
-public class Measurement implements Serializable{
+@Entity
+public class Measurement implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(length = 10, nullable = false, updatable = false)
-    private Double measure;
+  @Column(length = 10, nullable = false, updatable = false)
+  private Double measure;
 
-    @ManyToOne
-    @JoinColumn(name="sensor_id", nullable = false, updatable = false)
-    private Sensors sensor;
+  @ManyToOne
+  @JoinColumn(name = "sensor_id", nullable = false, updatable = false)
+  private Sensors sensor;
 }
--- src/main/java/com/college/hotlittlepigs/meansurement/MeasurementRepository.java
+++ src/main/java/com/college/hotlittlepigs/meansurement/MeasurementRepository.java
@@ -5,9 +5,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface MeasurementRepository extends JpaRepository<Measurement, Long>{
-    
-    public Page<Measurement> findAllBySensorId(Long id, Pageable pageable);
+public interface MeasurementRepository extends JpaRepository<Measurement, Long> {
+
+  Page<Measurement> findAllBySensorId(Long id, Pageable pageable);
 }
