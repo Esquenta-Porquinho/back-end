fb3d4ca2165bdf97064e5403a61ef2af0ffb9274
Kammradt <vinicius.kammradt1@gmail.com>
2020-11-15 15:18:56 -0300

    chore: :art: refactor on Matrix module

--- src/main/java/com/college/hotlittlepigs/matrix/MatrixResource.java
+++ src/main/java/com/college/hotlittlepigs/matrix/MatrixResource.java
@@ -1,72 +1,62 @@
 package com.college.hotlittlepigs.matrix;
 
-import java.util.Map;
-
-import javax.validation.Valid;
-
 import com.college.hotlittlepigs.matrix.dto.MatrixSaveDTO;
 import com.college.hotlittlepigs.model.PageModel;
 import com.college.hotlittlepigs.model.PageRequestModel;
-
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import lombok.AllArgsConstructor;
+import javax.validation.Valid;
+import java.util.Map;
 
 @AllArgsConstructor
 @RequestMapping(value = "matrix")
 @RestController
 public class MatrixResource {
-    
-    private MatrixService matrixService;
 
-    @Secured({ "ROLE_MANAGER", "ROLE_SIMPLE" })
-    @PreAuthorize("@logHandler.saveLog('Create new matrix')")
-    @PostMapping()
-    public ResponseEntity<Matrix> save(@RequestBody @Valid MatrixSaveDTO matrixSaveDTO){
-        Matrix matrix = matrixSaveDTO.toMatrix();
-        Matrix createdMatrix = matrixService.save(matrix);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdMatrix);
-    }
+  private final MatrixService service;
 
-    @Secured({ "ROLE_MANAGER", "ROLE_SIMPLE" })
-    @GetMapping()
-    public ResponseEntity<PageModel<Matrix>> listAll(@RequestParam Map<String, String> params){
-        PageRequestModel pr = new PageRequestModel(params);
-        PageModel<Matrix> pm = matrixService.listAll(pr);
-        return ResponseEntity.ok(pm);
-    }
+  @Secured({"ROLE_MANAGER", "ROLE_SIMPLE"})
+  @PreAuthorize("@logHandler.saveLog('Create new matrix')")
+  @PostMapping()
+  public ResponseEntity<Matrix> save(@RequestBody @Valid MatrixSaveDTO matrixSaveDTO) {
+    Matrix matrix = matrixSaveDTO.toMatrix();
+    Matrix createdMatrix = service.save(matrix);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdMatrix);
+  }
 
-    @Secured({ "ROLE_MANAGER", "ROLE_SIMPLE" })
-    @GetMapping("/{id}")
-    public ResponseEntity<Matrix> getById(@PathVariable("id") Long id){
-        Matrix matrix = matrixService.getById(id);
-        return ResponseEntity.ok(matrix);
-    }
+  @Secured({"ROLE_MANAGER", "ROLE_SIMPLE"})
+  @GetMapping()
+  public ResponseEntity<PageModel<Matrix>> listAll(@RequestParam Map<String, String> params) {
+    PageRequestModel pr = new PageRequestModel(params);
+    PageModel<Matrix> pm = service.listAll(pr);
+    return ResponseEntity.ok(pm);
+  }
 
-    @Secured({ "ROLE_MANAGER", "ROLE_SIMPLE" })
-    @PreAuthorize("@logHandler.saveLog('Deactivate matrix' + #id)")
-    @PatchMapping("/deactivate/{id}")
-    public ResponseEntity<Matrix> deactivate(@PathVariable("id") Long id) {
-        Matrix matrix = matrixService.updateStatus(id, false);
-        return ResponseEntity.ok(matrix);
-    }
+  @Secured({"ROLE_MANAGER", "ROLE_SIMPLE"})
+  @GetMapping("/{id}")
+  public ResponseEntity<Matrix> getById(@PathVariable("id") Long id) {
+    Matrix matrix = service.getById(id);
+    return ResponseEntity.ok(matrix);
+  }
 
-    @Secured({ "ROLE_MANAGER", "ROLE_SIMPLE" })
-    @PreAuthorize("@logHandler.saveLog('Activate matrix' + #id)")
-    @PatchMapping("/activate/{id}")
-    public ResponseEntity<Matrix> activate(@PathVariable("id") Long id) {
-        Matrix matrix = matrixService.updateStatus(id, true);
-        return ResponseEntity.ok(matrix);
-    }
+  @Secured({"ROLE_MANAGER", "ROLE_SIMPLE"})
+  @PreAuthorize("@logHandler.saveLog('Deactivate matrix' + #id)")
+  @PatchMapping("/deactivate/{id}")
+  public ResponseEntity<Matrix> deactivate(@PathVariable("id") Long id) {
+    Matrix matrix = service.updateStatus(id, false);
+    return ResponseEntity.ok(matrix);
+  }
+
+  @Secured({"ROLE_MANAGER", "ROLE_SIMPLE"})
+  @PreAuthorize("@logHandler.saveLog('Activate matrix' + #id)")
+  @PatchMapping("/activate/{id}")
+  public ResponseEntity<Matrix> activate(@PathVariable("id") Long id) {
+    Matrix matrix = service.updateStatus(id, true);
+    return ResponseEntity.ok(matrix);
+  }
 }
