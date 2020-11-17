ba203fe8775220571b709a8a7ac8392fd7995307
Kammradt <vinicius.kammradt1@gmail.com>
2020-11-17 00:19:27 -0300

    chore: :art: changes explicit type to var keyword

--- src/main/java/com/college/hotlittlepigs/user/UserService.java
+++ src/main/java/com/college/hotlittlepigs/user/UserService.java
@@ -10,7 +10,6 @@ import com.college.hotlittlepigs.user.dto.UserUpdateRoleDTO;
 import com.college.hotlittlepigs.user.enums.Role;
 import com.college.hotlittlepigs.user.util.HashUtil;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -22,10 +21,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.mail.internet.MimeMessage;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -37,11 +34,11 @@ public class UserService implements UserDetailsService {
 
   public int updateRole(UserUpdateRoleDTO user, Long id) {
 
-    Optional<User> result = repository.findById(id);
+    var result = repository.findById(id);
     if (result.isEmpty()) throw new NotFoundException("User not found !");
 
     if (!accessManager.isAdmin()) {
-      User checkUser = result.get();
+      var checkUser = result.get();
       if (checkUser.getRole() == Role.ADMIN || user.getRole() == Role.ADMIN)
         throw new AccessDeniedException("Access Denied");
     }
@@ -50,7 +47,7 @@ public class UserService implements UserDetailsService {
   }
 
   public User save(User user) {
-    String hash = HashUtil.getSecureHash(user.getPassword());
+    var hash = HashUtil.getSecureHash(user.getPassword());
     user.setPassword(hash);
     user.setRole(Role.ASPIRANT);
     return repository.save(user);
@@ -61,20 +58,20 @@ public class UserService implements UserDetailsService {
   }
 
   public User getById(Long id) {
-    Optional<User> result = repository.findById(id);
+    var result = repository.findById(id);
 
     return result.orElseThrow(() -> new UsernameNotFoundException("User not found"));
   }
 
   public User getByEmail(String email) {
-    Optional<User> result = repository.findByEmail(email);
+    var result = repository.findByEmail(email);
 
     return result.orElseThrow(() -> new UsernameNotFoundException("User not found"));
   }
 
   public PageModel<User> listAll(PageRequestModel pr) {
     Pageable pageable = pr.toSpringPageRequest();
-    Page<User> page = repository.findAll(pageable);
+    var page = repository.findAll(pageable);
 
     return new PageModel<>(
         (int) page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
@@ -82,7 +79,7 @@ public class UserService implements UserDetailsService {
 
   public PageModel<User> listAllNotAdmin(PageRequestModel pr) {
     Pageable pageable = pr.toSpringPageRequest();
-    Page<User> page = repository.findAllNotAdmin(Role.ADMIN, pageable);
+    var page = repository.findAllNotAdmin(Role.ADMIN, pageable);
 
     return new PageModel<>(
         (int) page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
@@ -90,7 +87,7 @@ public class UserService implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = getByEmail(username);
+    var user = getByEmail(username);
 
     List<GrantedAuthority> authorities =
         Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
@@ -100,8 +97,8 @@ public class UserService implements UserDetailsService {
   }
 
   public UserLoginServiceDTO login(org.springframework.security.core.userdetails.User userSpring) {
-    String email = userSpring.getUsername();
-    List<String> roles =
+    var email = userSpring.getUsername();
+    var roles =
         userSpring.getAuthorities().stream()
             .map(GrantedAuthority::getAuthority)
             .collect(Collectors.toList());
@@ -109,15 +106,15 @@ public class UserService implements UserDetailsService {
   }
 
   public void sendWarnings(Controller controller) {
-    List<User> users = repository.findAllByRole(Role.ADMIN);
+    var users = repository.findAllByRole(Role.ADMIN);
 
-    for (User user : users) {
-      JavaMailSenderImpl sender = new JavaMailSenderImpl();
+    for (var user : users) {
+      var sender = new JavaMailSenderImpl();
       sender.setHost("To define");
       sender.setPort(0);
 
-      MimeMessage message = sender.createMimeMessage();
-      MimeMessageHelper helper = new MimeMessageHelper(message);
+      var message = sender.createMimeMessage();
+      var helper = new MimeMessageHelper(message);
       try {
         helper.setFrom("Esquenta-Porquinho");
         helper.setSubject("Error on controller" + controller.getName());
