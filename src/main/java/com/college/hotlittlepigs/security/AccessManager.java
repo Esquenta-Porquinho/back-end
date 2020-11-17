002ba79600124d50eff05dd7976bf50cabc782ac
Kammradt <vinicius.kammradt1@gmail.com>
2020-11-17 00:43:58 -0300

    chore: :bug: uses userService with lazy injection

--- src/main/java/com/college/hotlittlepigs/security/AccessManager.java
+++ src/main/java/com/college/hotlittlepigs/security/AccessManager.java
@@ -2,15 +2,19 @@ package com.college.hotlittlepigs.security;
 
 import com.college.hotlittlepigs.user.UserService;
 import com.college.hotlittlepigs.user.enums.Role;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component("accessManager")
-@AllArgsConstructor
 public class AccessManager {
 
   private final UserService userService;
+
+  public AccessManager(@Lazy UserService userService) {
+    this.userService = userService;
+  }
 
   public boolean isOwner(Long id) {
     var email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
