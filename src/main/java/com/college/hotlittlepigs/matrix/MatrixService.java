69bfcc58f50b3aca357cd5aca33e344019b83d35
Kammradt <vinicius.kammradt1@gmail.com>
2020-11-17 00:18:58 -0300

    chore: :art: changes explicit type to var keyword

--- src/main/java/com/college/hotlittlepigs/matrix/MatrixService.java
+++ src/main/java/com/college/hotlittlepigs/matrix/MatrixService.java
@@ -4,11 +4,8 @@ import com.college.hotlittlepigs.matrix.exception.MatrixNotFoundException;
 import com.college.hotlittlepigs.pagination.PageModel;
 import com.college.hotlittlepigs.pagination.PageRequestModel;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -21,21 +18,21 @@ public class MatrixService {
 
   public PageModel<Matrix> listAll(PageRequestModel pr) {
     Pageable pageable = pr.toSpringPageRequest();
-    Page<Matrix> page = repository.findAll(pageable);
+    var page = repository.findAll(pageable);
 
     return new PageModel<>(
         (int) page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
   }
 
   public Matrix getById(Long id) {
-    Optional<Matrix> result = repository.findById(id);
+    var result = repository.findById(id);
     if (result.isEmpty()) throw new MatrixNotFoundException();
 
     return result.get();
   }
 
   public Matrix updateStatus(Long id, Boolean status) {
-    Matrix matrix = this.getById(id);
+    var matrix = this.getById(id);
     matrix.setStatus(status);
     return save(matrix);
   }
