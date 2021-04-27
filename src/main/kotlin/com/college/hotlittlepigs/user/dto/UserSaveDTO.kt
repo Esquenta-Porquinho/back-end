9ea24b5a9d159357c1a8355d08ce140d03bc4fba
kammradt <vinicius.kammradt1@gmail.com>
2021-04-26 23:20:51 -0300

    chore: :art: migrate UserSaveDTO to dataclass

--- src/main/kotlin/com/college/hotlittlepigs/user/dto/UserSaveDTO.kt
+++ src/main/kotlin/com/college/hotlittlepigs/user/dto/UserSaveDTO.kt
@@ -5,15 +5,10 @@ import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
-class UserSaveDTO {
-    @NotBlank(message = "Name is Required")
-    lateinit var name: String
-
-    @Email(message = "Invalid email address")
-    lateinit var email: String
-
-    @Size(min = 8, max = 16, message = "Password size must be between 8 and 16")
-    lateinit var password: String
-
+data class UserSaveDTO(
+        @NotBlank(message = "Name is Required")val name: String,
+        @Email(message = "Invalid email address") val email: String,
+        @Size(min = 8, max = 16, message = "Password size must be between 8 and 16") val password: String
+) {
     fun toUser(): User = User(name, email, password)
 }
